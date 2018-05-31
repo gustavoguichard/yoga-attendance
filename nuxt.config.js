3d2bcced03ca6f6ce5a8e4551c071d3539a3b0eb
Gustavo Guichard <gustavoguichard@gmail.com>
2018-05-31 12:06:21 -0300

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Attendance management for a Yoga school' }
+      { hid: 'description', name: 'description', content: 'Attendance management for a Yoga school - CIYF' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
