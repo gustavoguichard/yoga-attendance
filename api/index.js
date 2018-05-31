d705d899255f90f355a3389d9b8bb06b83b54220
Gustavo Guichard <gustavoguichard@gmail.com>
2018-05-31 14:54:36 -0300

    Back to axios

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import feathers from '@feathersjs/feathers'
 import auth from '@feathersjs/authentication-client'
 import rest from '@feathersjs/rest-client'
-import axios from 'axios'
 import storage from '@/utils/feathers-storage'
+import axios from 'axios'
 
 const restClient = rest(process.env.baseUrl)
 const api = feathers()
