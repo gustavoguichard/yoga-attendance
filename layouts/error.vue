001a302b83f60ac8f3d704db608a0059e51171f7
Gustavo Guichard <gustavoguichard@gmail.com>
2018-06-02 19:17:52 -0300

    Custom status message for error pages

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,5 @@
 <template>
   <v-app id="ciyf-app" class="is-404">
-    <application-controls />
     <v-container class="app-container" fluid fill-height>
       <v-layout justify-center class="page-container">
         <v-card class="main-card mt-3 py-4 px-2">
@@ -8,8 +7,8 @@
             <v-card-text class="text-xs-center">
               <h1 class="display-4">{{ error.statusCode }}</h1>
               <img src="/logo.png" alt="Iyengar Brasil" width="150" class="my-4" />
-              <p class="title">Página inválida.</p>
-              <p class="body-2">Por favor, tente outra página ou volte mais tarde.</p>
+              <p class="title">{{ message.title }}.</p>
+              <p class="body-2">{{ message.desc }}.</p>
             </v-card-text>
           </v-layout>
         </v-card>
@@ -19,12 +18,18 @@
 </template>
 
 <script>
-import applicationControls from '@/components/application-controls'
+import { inRange } from 'lodash'
 
 export default {
   name: 'nuxt-error',
   props: ['error'],
-  components: { applicationControls },
+  computed: {
+    message() {
+      return inRange(this.error.statusCode, 400, 500)
+        ? { title: 'Página inválida', desc: 'Esta página não existe no momento, tente navegar pelas outras páginas do sistema' }
+        : { title: 'Erro interno', desc: 'Ocorreu algum problema no sistema. Por favor, tente atualizar a página e se o problema persistir, contate o responsável' }
+    },
+  },
 };
 </script>
 
