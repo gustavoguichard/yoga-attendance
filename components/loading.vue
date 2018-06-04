6adad1a5ebf336c4d249cf70e362944294392fd4
Gustavo Guichard <gustavoguichard@gmail.com>
2018-06-03 22:45:07 -0300

    Loading on center of screen

--- components/loading.vue
+++ components/loading.vue
@@ -1,6 +1,8 @@
 <template>
   <div v-if="active" class="loading-container">
+    <div class="progress-wrapper" />
     <v-progress-circular
+      class="progress"
       :size="100"
       :width="15"
       indeterminate
@@ -16,14 +18,25 @@ export default { props: ['active'] }
 <style scoped>
 .loading-container {
   align-items: center;
-  background: rgba(255, 255, 255, .7);
   bottom: 0;
   display: flex;
   justify-content: center;
   left: 0;
+  max-height: 100vh;
   position: absolute;
   right: 0;
   top: 0;
   z-index: 2;
+}
+.progress {
+  position: fixed;
+}
+.progress-wrapper {
+  background: rgba(255, 255, 255, .7);
+  bottom: 0;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
 }
 </style>
