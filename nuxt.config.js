8d488d4ad5161766cf28a3340417d5ba6a3df42b
Gustavo Guichard <gustavoguichard@gmail.com>
2018-06-04 12:54:03 -0300

    Created manifest.json

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,14 +14,14 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'apple-mobile-web-app-title', content: 'CIYF' },
-      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      { name: 'mobile-web-app-capable', content: 'yes' },
       { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
       { hid: 'description', name: 'description', content: 'Gerenciamento de presenças e pagamentos para uma escola de Yoga - CIYF' }
     ],
     link: [
+      { rel: 'manifest', type: 'application/manifest+json', href: '/manifest.json' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
-      { rel: 'apple-touch-startup-image', href: '/launch.png' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' },
     ]
   },
