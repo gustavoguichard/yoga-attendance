e36ee57f090fb34cfbbe651c448648b052e76894
Gustavo Guichard <gustavoguichard@gmail.com>
2018-07-05 22:09:01 -0300

    Removes loader from sign-in screen

--- layouts/minimal.vue
+++ layouts/minimal.vue
@@ -5,7 +5,6 @@
     </no-ssr>
     <v-content>
       <v-container class="app-container" fluid fill-height>
-        <loading :active="loading"></loading>
         <nuxt />
       </v-container>
     </v-content>
@@ -14,15 +13,10 @@
 
 <script>
 import notificationCenter from '@/components/notification-center'
-import loading from '@/components/loading'
-import { mapGetters } from 'vuex'
 
 export default {
   name: 'minimal',
-  components: { loading, notificationCenter },
-  computed: {
-    ...mapGetters('ui', ['loading']),
-  },
+  components: { notificationCenter },
   mounted() {
     this.$store.dispatch('ui/done')
   },
