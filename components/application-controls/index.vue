b2c70c7349ad6f232bf1001334cc5d13d83bd043
Gustavo Guichard <gustavoguichard@gmail.com>
2018-08-16 11:08:54 -0300

    Fix mobile toolbar

--- components/application-controls/index.vue
+++ components/application-controls/index.vue
@@ -6,12 +6,10 @@
     <v-toolbar color="grey darken-3" dark fixed app clipped-left>
       <v-toolbar-side-icon v-show="isMobile" @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title v-show="isMobile">CIYF</v-toolbar-title>
-      <template v-show="!isMobile">
-        <v-avatar size="36" class="ml-2">
-          <img src="/icon.png" alt="Logo" />
-        </v-avatar>
-        <v-toolbar-title>Centro Iyengar Yoga Florianópolis</v-toolbar-title>
-      </template>
+      <v-avatar v-show="!isMobile" size="36" class="ml-2">
+        <img src="/icon.png" alt="Logo" />
+      </v-avatar>
+      <v-toolbar-title v-show="!isMobile">Centro Iyengar Yoga Florianópolis</v-toolbar-title>
       <v-spacer></v-spacer>
       <syncer />
       <user-menu />
